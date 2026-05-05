72925b93d41d626c0bce4046eb8187980def5e80
Maksym Davydiuk <krytan53@gmail.com>
2026-05-06 02:40:10 +0300

    Implement addOne and add100 counter functions

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,11 @@ import { useState } from 'react';
 import './App.scss';
 
 export const App = () => {
-  const [count] = useState(0);
+  const [count, setCount] = useState(0);
 
-  const addOne = () => {
-    // write code here
-  };
+  const addOne = () => setCount(c => c + 1);
 
-  const add100 = () => {
-    // write code here
-  };
+  const add100 = () => setCount(c => c + 100);
 
   // DON'T change the code below
   const increase = () => {
